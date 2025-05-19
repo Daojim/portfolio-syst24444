7552202643cfce7b41aa3fb79ff1d2164e8fae2d
Daojim <jimmyddao@gmail.com>
2025-05-19 15:04:45 -0400

    Add content for about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,8 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  firstSection =
+    "Hello World, I'm Jimmy. I'm a Software Development student at Sheridan College, but I never had a background in programming prior.";
+  secondSection =
+    "I'm in the middle of a career transition where I used to work in the HVAC industry as well as many years in retail.";
+  thirdSection =
+    'The reason for my transition was because I suffered a lot of injuries in physical labour, so I needed to find another career path that satiated thirst for knowledge and constant need of self improvement.';
+  fourthSection =
+    "I took a chance with signing up for this program and so far I'm loving the challenges that I've come across with programming. I look forward to learn as much technologies as possible";
 }
