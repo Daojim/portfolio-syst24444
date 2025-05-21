4e49f3c48b19522743843ab1c07bcaca75ab74c2
Daojim <jimmyddao@gmail.com>
2025-05-21 15:37:42 -0400

    Change footer content

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   imports: [],
-  template: ` <p>footer works!</p> `,
+  template: `<ul>Technologies used:
+    <li>Angular</li>
+  </ul>`,
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {}
