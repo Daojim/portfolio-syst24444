31ad0b3b832c8a94f30c664c93629fef5fce794e
Daojim <jimmyddao@gmail.com>
2025-05-23 19:23:22 -0400

    Add material card module in project.component.html

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../modules/material-ui.module';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [MaterialModule, MatCardModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
-export class ProjectsComponent {
-
-}
+export class ProjectsComponent {}
