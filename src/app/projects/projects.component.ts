8c985c9c3782a33e2458bb96f496db840bab7579
Daojim <jimmyddao@gmail.com>
2025-05-23 19:38:59 -0400

    Change class name for material cards

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../modules/material-ui.module';
 import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [MaterialModule, MatCardModule],
+  imports: [MaterialModule, MatCardModule, CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
