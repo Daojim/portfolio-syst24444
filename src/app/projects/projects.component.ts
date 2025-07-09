26b03e061a2a3dec3b2dea4768755439bee2ae07
Daojim <jimmyddao@gmail.com>
2025-07-09 15:19:13 -0400

    Add like button to project cards

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,15 +3,23 @@ import { MaterialModule } from '../modules/material-ui.module';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { Projects } from '../interfaces/project-interfaces';
+import { CardbuttonsService } from '../services/cardbuttons.service';
 
 @Component({
   selector: 'app-projects',
   imports: [MaterialModule, MatCardModule, CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
+  providers: [CardbuttonsService],
 })
 export class ProjectsComponent {
   @Input() darkMode: boolean = false;
 
   @Input() projects!: Projects[];
+
+  constructor(private cardService: CardbuttonsService) {}
+
+  like() {
+    this.cardService.likeCard();
+  }
 }
