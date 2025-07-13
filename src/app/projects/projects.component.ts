43e3028b943d81c1314e313484417796eb01a515
Daojim <jimmyddao@gmail.com>
2025-07-13 14:54:23 -0400

    Add filter input to projects component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -4,22 +4,38 @@ import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { Projects } from '../interfaces/project-interfaces';
 import { CardbuttonsService } from '../services/cardbuttons.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-projects',
-  imports: [MaterialModule, MatCardModule, CommonModule],
+  imports: [MaterialModule, MatCardModule, CommonModule, FormsModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
   providers: [CardbuttonsService],
 })
 export class ProjectsComponent {
   @Input() darkMode: boolean = false;
-
   @Input() projects!: Projects[];
 
   constructor(private cardService: CardbuttonsService) {}
 
   like(elem: any) {
     this.cardService.likeCard(elem);
+  }
+
+  filteredData: Projects[] = [];
+  inputText: string = '';
+
+  filterProjects() {
+    if (this.inputText === '') {
+      this.filteredData = this.projects;
+    } else {
+      this.filteredData = [];
+      for (let project of this.projects) {
+        if (project.title.toLowerCase() === this.inputText.toLowerCase()) {
+          this.filteredData.push(project);
+        }
+      }
+    }
   }
 }
